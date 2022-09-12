b0b9c3b2050ba02efae4cb86d76f821a667de0ef
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-09-12 15:56:46 +0200

    feat(calibre-web): Add image based on onedr0p/ubuntu

---
+++ apps/calibre-web/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#shellcheck disable=SC1091
+test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
+
+#shellcheck disable=SC2086
+exec \
+  /usr/bin/python3  \
+    /app/cps.py \
+    "$@"

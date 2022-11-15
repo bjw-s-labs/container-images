bdafddcecdbc49921c23e9ef22b80e626082141e
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-11-15 09:47:54 +0100

    feat(gatus): Add custom image

---
+++ apps/gatus/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ -n "${GATUS_CONFIG_FILE_URL}" ]; then
+  echo "[INFO] Trying to download remote configuration from ${GATUS_CONFIG_FILE_URL}"
+  CURL_RESPONSE=$(curl -s -o "${GATUS_CONFIG_FILE}" -w "%{http_code}" -L "${GATUS_CONFIG_FILE_URL}")
+  if [ ! "${CURL_RESPONSE}" == "200" ]; then
+    echo "[ERROR] Failed to download remote configuration (HTTP error ${CURL_RESPONSE})"
+    exit 1
+  fi
+fi
+
+if [ ! -f "${GATUS_CONFIG_FILE}" ]; then
+  echo "[INFO] Copying default configuration to ${GATUS_CONFIG_FILE}"
+  cp "/app/config.default.yaml" "${GATUS_CONFIG_FILE}"
+fi
+
+#shellcheck disable=SC2086
+exec \
+  /app/gatus \
+    "$@"

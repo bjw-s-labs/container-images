437c829f8745514aa379d83eda72f2a39f6c4af8
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-12-06 07:53:47 +0100

    Log to stdout

--- apps/calibre-web/entrypoint.sh
+++ apps/calibre-web/entrypoint.sh
@@ -3,6 +3,12 @@
 #shellcheck disable=SC1091
 test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
 
+if [ -f "${CALIBRE_DBPATH}/calibre-web.log" ]; then
+  rm "${CALIBRE_DBPATH}/calibre-web.log"
+fi
+
+ln -s /dev/stdout "${CALIBRE_DBPATH}/calibre-web.log"
+
 #shellcheck disable=SC2086
 exec \
   /usr/bin/python3  \

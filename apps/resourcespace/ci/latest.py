748687ebaa0feed890c20af1acc6cdd810536e0b
Bernd Schorgers <me@bjw-s.dev>
2023-10-02 14:20:08 +0200

    feat(resourcespace): Add WIP

---
+++ apps/resourcespace/ci/latest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import svn.remote
+
+# Get the latest version of resourcespace
+
+repoURL = "https://svn.resourcespace.com/svn/rs/releases"
+
+def get_latest(channel):
+    releases = []
+    r = svn.remote.RemoteClient(repoURL)
+    releasesRemote = r.list(extended=True)
+
+    for entry in releasesRemote:
+        if entry["kind"] == "dir":
+            releases.append(float(entry["name"]))
+
+    return sorted(releases)[-1]
+
+if __name__ == "__main__":
+    import sys
+    channel = sys.argv[1]
+    print(get_latest(channel))
---
+++ apps/resourcespace/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [ ! -f "${RADICALE_CONFIG_FILE}" ]; then
+  echo "[INFO] Copying default configuration to ${RADICALE_CONFIG_FILE}"
+  cp "/app/config.default" "${RADICALE_CONFIG_FILE}"
+fi
+
+#shellcheck disable=SC2086
+exec \
+  radicale --config "${RADICALE_CONFIG_FILE}" \
+    "$@"

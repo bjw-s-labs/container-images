5d3f17be08fcf531bb4c7e19cd4ed3f7a47b3100
Bernd Schorgers <me@bjw-s.dev>
2023-10-25 15:10:14 +0200

    ci: Update workflows

--- .ci/scripts/render-readme.py
+++ .ci/scripts/render-readme.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python3
 import os
 import json
 import requests
 import yaml
 
-from jinja2 import Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 env = Environment(
-    loader=PackageLoader("render-readme"),
+    loader=FileSystemLoader(".ci/templates"),
     autoescape=select_autoescape()
 )
 

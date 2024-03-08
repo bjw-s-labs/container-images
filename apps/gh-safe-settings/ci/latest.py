b03263ba6f09b50bdf79bbdeb3fc3c9bb07061cc
Bernd Schorgers <me@bjw-s.dev>
2024-03-08 09:24:57 +0100

    fix(gh-safe-settings): Pin to stable versions

--- apps/gh-safe-settings/ci/latest.py
+++ apps/gh-safe-settings/ci/latest.py
@@ -2,16 +2,27 @@
 
 import requests
 import json
+import semver
 
 # Get the latest version of radicale
 
-URL = "https://api.github.com/repos/github/safe-settings/releases/latest"
+URL = "https://api.github.com/repos/github/safe-settings/releases"
 
 def get_latest(channel):
     r = requests.get(URL)
     data = json.loads(r.text)
-    version = data['tag_name'].removeprefix("v")
-    return version
+
+    latest_version = ""
+
+    if channel == "stable":
+        versions = [ semver.Version.parse(release['tag_name']) for release in data ]
+        stable_versions = [ version for version in versions if not version.prerelease]
+        latest_version = max(stable_versions)
+    else:
+        versions = [ semver.Version.parse(release['tag_name']) for release in data ]
+        latest_version = max(versions)
+
+    return str(latest_version).removeprefix("v")
 
 if __name__ == "__main__":
     import sys

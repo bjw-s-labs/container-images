455d0447e985e21e69916927ab690be435b7938a
Bernd Schorgers <me@bjw-s.dev>
2024-04-09 13:56:06 +0200

    fix(calibre-web)!: Alpine base

--- apps/calibre-web/entrypoint.sh
+++ apps/calibre-web/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-#shellcheck disable=SC1091
-test -f "/scripts/umask.sh" && source "/scripts/umask.sh"
-
 if [ -f "${CALIBRE_DBPATH}/calibre-web.log" ]; then
   rm "${CALIBRE_DBPATH}/calibre-web.log"
 fi
@@ -11,6 +8,6 @@ ln -s /dev/stdout "${CALIBRE_DBPATH}/calibre-web.log"
 
 #shellcheck disable=SC2086
 exec \
-  /usr/bin/python3  \
+  python3  \
     /app/cps.py \
     "$@"

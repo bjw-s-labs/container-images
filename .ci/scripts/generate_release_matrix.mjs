2ca769ca9d4198049e2416bfdd6d4239d3fcd2e0
Bernd Schorgers <me@bjw-s.dev>
2024-09-16 13:40:08 +0200

    Testing 1,2,3

--- .ci/scripts/generate_release_matrix.mjs
+++ .ci/scripts/generate_release_matrix.mjs
@@ -1,26 +1,20 @@
 /**
- * generate_matrix.js
+ * generate_release_matrix.js
  *
  * This script generates a matrix of Docker images to build and push to a Container Registry.
  * It performs the following tasks:
  *
- * 1. **Extracts Tool Versions**:
- *    - Reads `Dockerfile` in each subdirectory under the `containers` directory.
- *    - Extracts the version of the tool specified in the `ARG VERSION` line of the `Dockerfile`.
- *    - Throws an error if the `ARG VERSION` line is missing or cannot be parsed.
- *
- * 2. **Checks if Image Version Exists**:
- *    - Uses the GitHub REST API to check if a Docker image with the specified version already exists on GHCR.
- *    - Retrieves all available versions of the package from GHCR.
- *    - Compares the extracted version with the list of existing versions to determine if it needs to be built.
+ * 1. **Extracts metadata**:
+ *    - Reads `metadata.yaml` in each subdirectory under the `containers` directory.
  *
  * 3. **Generates a Build Matrix**:
  *    - Constructs a matrix of images to build based on the versions that do not already exist on GHCR.
  *    - Formats the matrix in JSON to be used by GitHub Actions for subsequent build jobs.
  *
  * **Environment Variables**:
- * - `GITHUB_OWNER`: The GitHub Container Registry images owner
  * - `GITHUB_TOKEN`: A GitHub token with permissions to access the GHCR and the repository.
+ * - `IMAGES_FOLDER`: The base folder that contains the container images. Defaults to "apps".
+ * - `INCLUDE_IMAGES`: An optional comma-separated list of images to include in the matrix.
  *
  * **Usage**:
  * - This is meant to be ran in a GitHub Actions workflow to generate a matrix for a downstream job.
@@ -32,7 +26,6 @@ import YAML from "yaml";
 import { Octokit } from "@octokit/rest";
 
 // Configuration from environment variables
-const GITHUB_OWNER = process.env.GITHUB_REPOSITORY.split("/")[0];
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const IMAGES_FOLDER = process.env.IMAGE_FOLDER || "apps";
 const INCLUDE_IMAGES = process.env.INCLUDE_IMAGES;
@@ -66,50 +59,6 @@ function extractPlatforms(metadataPath) {
 function extractTestConfig(metadataPath) {
   return extractMetadataField(metadataPath, "tests");
 }
-
-// async function isGithubOrg(owner) {
-//     try {
-//         const response = await octokit.request('GET /users/{owner}', {
-//             owner: owner,
-//             headers: {
-//                 'X-GitHub-Api-Version': '2022-11-28'
-//             }
-//         });
-//         return (response.data.type === 'Organization')
-//     } catch (error) {
-//         return false
-//     }
-// }
-
-// async function imageExists(image_name, version) {
-//     var packages_url = 'GET /users/{org}/packages/container/{package_name}/versions'
-//     if (await isGithubOrg(GITHUB_OWNER)) {
-//         packages_url = 'GET /orgs/{org}/packages/container/{package_name}/versions'
-//     }
-
-//     try {
-//         const response = await octokit.request(packages_url, {
-//             org: GITHUB_OWNER,
-//             package_name: image_name,
-//             headers: {
-//                 'X-GitHub-Api-Version': '2022-11-28'
-//             }
-//         });
-//         for (const ver of response.data) {
-//             if (ver.metadata.container.tags.includes(version)) {
-//                 return true
-//             }
-//         }
-//         return false
-//     } catch (error) {
-//         return false
-//     }
-// }
-
-// function getPullRequestNumber(ref) {
-//   const match = ref.match(/^refs\/pull\/(\d+)\/merge$/);
-//   return match ? match[1] : null;
-// }
 
 async function generateMatrix() {
   const basePath = IMAGES_FOLDER;

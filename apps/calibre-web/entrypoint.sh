c437608ad05f79e894f454300774e88a2ae2325b
Bernd Schorgers <me@bjw-s.dev>
2024-12-26 11:48:36 +0100

    feat(calibre-web)!: Migrate to running as `nobody`

--- apps/calibre-web/entrypoint.sh
+++ apps/calibre-web/entrypoint.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-if [ -f "${CALIBRE_DBPATH}/calibre-web.log" ]; then
-  rm "${CALIBRE_DBPATH}/calibre-web.log"
-fi
-
-ln -s /dev/stdout "${CALIBRE_DBPATH}/calibre-web.log"
-
 #shellcheck disable=SC2086
 exec \
   python3  \
     /app/cps.py \
+    -o /dev/stdout \
     "$@"

391ee3ffaca2d0716515d5baeeabd398cb009840
Bernd Schorgers <me@bjw-s.dev>
2026-04-01 23:05:13 +0200

    feat(forgejo-runner): Update baseimage, tests

--- apps/forgejo-runner/container_test.go
+++ apps/forgejo-runner/container_test.go
@@ -11,7 +11,23 @@ func Test(t *testing.T) {
 	ctx := context.Background()
 	image := testhelpers.GetTestImage("ghcr.io/bjw-s-labs/forgejo-rolling:rolling")
 
+	t.Run("Check flux exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/flux", nil)
+	})
+
 	t.Run("Check flux-local exists", func(t *testing.T) {
-		testhelpers.TestFileExists(t, ctx, image, "/home/runner/.local/bin/flux-local", nil)
+		testhelpers.TestFileExists(t, ctx, image, "/root/.local/bin/flux-local", nil)
+	})
+
+	t.Run("Check helm exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/helm", nil)
+	})
+
+	t.Run("Check kustomize exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/kustomize", nil)
+	})
+
+	t.Run("Check rsync exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/bin/rsync", nil)
 	})
 }
